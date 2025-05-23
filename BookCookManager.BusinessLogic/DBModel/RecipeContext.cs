79333368e19c25ee804bfdff77c98cdf0dd8ec10
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-05-23 21:42:31 +0300

    fixed some errors

---
+++ BookCookManager.BusinessLogic/DBModel/RecipeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookCookManager.Domain.Entities;
+
+namespace BookCookManager.BusinessLogic.DBModel
+{
+    public class RecipeContext : DbContext
+    {
+
+    }
+}
